e940e6c4d4089ed710079222e4218ac4422cd2c2
Subhankar Biswal <subhankarbiswal57@gmail.com>
2026-09-12 16:07:03 +0530

    Add Week 6 Assignment Problem 1: Library Inventory Management

--- PROGRAM1.java
+++ PROGRAM1.java
@@ -1,30 +1,35 @@
-import java.util.Scanner;
-
+/**
+ * Week 6 Assignment - Problem 1: Library Inventory Management
+ * Refactors parallel arrays into an OOP design with BookInventory class.
+ */
 public class PROGRAM1 {
+
+    static class BookInventory {
+        private String title;
+        private String author;
+        private int copiesAvailable;
+
+        public BookInventory(String title, String author, int copiesAvailable) {
+            this.title = title;
+            this.author = author;
+            this.copiesAvailable = copiesAvailable;
+        }
+
+        public void printEntry() {
+            System.out.println(title + " by " + author + " - " + copiesAvailable + " copies available");
+        }
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = sc.nextInt();
+        BookInventory[] books = {
+            new BookInventory("Clean Code", "Robert C. Martin", 3),
+            new BookInventory("Effective Java", "Joshua Bloch", 5),
+            new BookInventory("Refactoring", "Martin Fowler", 0),
+            new BookInventory("Design Patterns", "GoF", 2)
+        };
 
-        boolean isPrime = true;
-
-        for (int i = 2; i < number; i++) {
-            if (number % i == 0) {
-                isPrime = false;
-                break;
-            }
+        for (BookInventory book : books) {
+            book.printEntry();
         }
-
-        if (number < 2) {
-            isPrime = false;
-        }
-
-        if (isPrime) {
-            System.out.println("Is the number " + number + " a Prime number? Yes");
-        } else {
-            System.out.println("Is the number " + number + " a Prime number? No");
-        }
-
-        sc.close();
     }
 }
