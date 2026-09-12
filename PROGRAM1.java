beabe57357ddc0513f0171e610beac7f0b5ee793
Subhankar Biswal <subhankarbiswal57@gmail.com>
2026-09-12 16:04:34 +0530

    Add Week 6 Practice Problem 1: Employee Payroll System

--- PROGRAM1.java
+++ PROGRAM1.java
@@ -1,30 +1,76 @@
-import java.util.Scanner;
-
+/**
+ * Week 6 Practice - Problem 1: Employee Payroll System (Inheritance + Polymorphism)
+ * Base class Employee with subclasses FullTimeEmployee and PartTimeEmployee.
+ */
 public class PROGRAM1 {
+
+    static abstract class Employee {
+        private String name;
+        private int id;
+
+        public Employee(String name, int id) {
+            this.name = name;
+            this.id = id;
+        }
+
+        public String getName() { return name; }
+        public int getId() { return id; }
+
+        public abstract double calculateSalary();
+
+        @Override
+        public String toString() {
+            return "ID: " + id + " | Name: " + name + " | Salary: " + String.format("%.2f", calculateSalary());
+        }
+    }
+
+    static class FullTimeEmployee extends Employee {
+        private double monthlySalary;
+
+        public FullTimeEmployee(String name, int id, double monthlySalary) {
+            super(name, id);
+            this.monthlySalary = monthlySalary;
+        }
+
+        @Override
+        public double calculateSalary() {
+            return monthlySalary;
+        }
+    }
+
+    static class PartTimeEmployee extends Employee {
+        private double hourlyRate;
+        private int hoursWorked;
+
+        public PartTimeEmployee(String name, int id, double hourlyRate, int hoursWorked) {
+            super(name, id);
+            this.hourlyRate = hourlyRate;
+            this.hoursWorked = hoursWorked;
+        }
+
+        @Override
+        public double calculateSalary() {
+            return hourlyRate * hoursWorked;
+        }
+    }
+
+    public static void printPayroll(Employee[] employees) {
+        double totalPayroll = 0;
+        System.out.println("--- Payroll Report ---");
+        for (Employee emp : employees) {
+            System.out.println(emp);
+            totalPayroll += emp.calculateSalary();
+        }
+        System.out.println("Total Payroll: " + String.format("%.2f", totalPayroll));
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = sc.nextInt();
-
-        boolean isPrime = true;
-
-        for (int i = 2; i < number; i++) {
-            if (number % i == 0) {
-                isPrime = false;
-                break;
-            }
-        }
-
-        if (number < 2) {
-            isPrime = false;
-        }
-
-        if (isPrime) {
-            System.out.println("Is the number " + number + " a Prime number? Yes");
-        } else {
-            System.out.println("Is the number " + number + " a Prime number? No");
-        }
-
-        sc.close();
+        Employee[] team = {
+            new FullTimeEmployee("Alice", 101, 50000),
+            new PartTimeEmployee("Bob", 102, 250, 80),
+            new FullTimeEmployee("Charlie", 103, 60000),
+            new PartTimeEmployee("Diana", 104, 300, 60)
+        };
+        printPayroll(team);
     }
 }
