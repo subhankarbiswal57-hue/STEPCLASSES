6c8c8ace5a6c534a30f4e9b1fb849a0746e14635
Subhankar Biswal <subhankarbiswal57@gmail.com>
2026-09-12 16:07:05 +0530

    Add Week 6 Assignment Problem 3: Employee Profile Creation

--- PROGRAM3.java
+++ PROGRAM3.java
@@ -1,26 +1,37 @@
-import java.util.Scanner;
+/**
+ * Week 6 Assignment - Problem 3: Employee Profile Creation
+ * Constructor chaining with this(...) for Employee class supporting interns & permanent staff.
+ */
 public class PROGRAM3 {
+
+    static class Employee {
+        private String empId;
+        private String empName;
+        private double salary;
+        private boolean isIntern;
+
+        public Employee(String empId, String empName, double salary) {
+            this.empId = empId;
+            this.empName = empName;
+            this.salary = salary;
+            this.isIntern = false;
+        }
+
+        public Employee(String empId, String empName) {
+            this(empId, empName, 0.0);
+            this.isIntern = true;
+        }
+
+        public void printProfile() {
+            System.out.println(empId + " | " + empName + " | Rs " + salary + " | Intern: " + isIntern);
+        }
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = sc.nextInt();
-        int origNumber = number;
+        Employee permanent = new Employee("E-101", "Divya", 65000.0);
+        Employee intern = new Employee("E-102", "Arjun");
 
-        int reversedNumber = 0;
-
-        while (number != 0) {
-            int digit = number % 10;
-            reversedNumber = reversedNumber * 10 + digit;
-            number = number / 10;
-        }
-
-        if (reversedNumber == origNumber) {
-            System.out.println("Is the number " + origNumber + " a Palindrome? Yes");
-        } else {
-            System.out.println("Is the number " + origNumber + " a Palindrome? No");
-        }
-
-        sc.close();
+        permanent.printProfile();
+        intern.printProfile();
     }
 }
-    
